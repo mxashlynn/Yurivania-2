32831e0d79109177f53ef3538310ff4fe833676b
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-06-08 15:26:36 -0700

    Creates GB Studio and Tiled projects.

---
+++ design/tiles/yurivania-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Yurivania 2" tilewidth="8" tileheight="8" tilecount="432" columns="24" backgroundcolor="#65ff00" objectalignment="center">
+ <image source="tile_pattern_table.png" trans="65ff00" width="192" height="144"/>
+</tileset>
