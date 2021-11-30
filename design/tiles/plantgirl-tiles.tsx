d8aa5bf4c9d271269b53e949af80218dca086a44
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-11-29 20:33:02 -0800

    Fixes sizing error.

--- design/tiles/plantgirl-tiles.tsx
+++ design/tiles/plantgirl-tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Plantgirl Tiles" tilewidth="8" tileheight="8" tilecount="30" columns="5">
- <image source="plantgirl-tiles.png" trans="65ff00" width="40" height="48"/>
+<tileset version="1.5" tiledversion="1.7.2" name="Plantgirl Tiles" tilewidth="8" tileheight="8" tilecount="49" columns="7">
+ <image source="plantgirl-tiles.png" trans="65ff00" width="56" height="56"/>
 </tileset>
