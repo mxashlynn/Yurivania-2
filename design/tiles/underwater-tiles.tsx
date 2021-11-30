26bf863b0148cde0b476a76c7ff3cfde6a41f060
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-11-29 20:33:46 -0800

    Finishes laying out tiles for Echoing Vaults.

---
+++ design/tiles/underwater-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Underwater Tiles" tilewidth="8" tileheight="8" tilecount="16" columns="8">
+ <image source="underwater-tiles.png" trans="65ff00" width="64" height="16"/>
+</tileset>
