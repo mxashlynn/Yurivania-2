15a63fc683b3180e9e815657f98faf74cd03d2b2
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-12-02 17:24:34 -0800

    Fixes error in tile sheet definition.

--- design/tiles/coffin-tiles.tsx
+++ design/tiles/coffin-tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="coffin-tiles" tilewidth="8" tileheight="8" tilecount="18" columns="9">
- <image source="coffin-tiles.png" trans="65ff00" width="72" height="16"/>
+<tileset version="1.5" tiledversion="1.7.2" name="coffin-tiles" tilewidth="8" tileheight="8" tilecount="16" columns="8">
+ <image source="coffin-tiles.png" trans="65ff00" width="64" height="16"/>
 </tileset>
