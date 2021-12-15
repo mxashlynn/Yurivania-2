17b5d1f0c70e0e440ffac71c05de48b2381ef603
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-12-15 14:04:11 -0800

    Replaces actor-based coffins with backgrounds in Old Vampire Quarter W.
    Fixes #3 Talking Sometimes Fails.

--- design/tiles/coffin-tiles.tsx
+++ design/tiles/coffin-tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="coffin-tiles" tilewidth="8" tileheight="8" tilecount="16" columns="8">
- <image source="coffin-tiles.png" trans="65ff00" width="64" height="16"/>
+<tileset version="1.5" tiledversion="1.7.2" name="coffin-tiles" tilewidth="8" tileheight="8" tilecount="22" columns="8">
+ <image source="coffin-tiles.png" trans="65ff00" width="88" height="16"/>
 </tileset>
