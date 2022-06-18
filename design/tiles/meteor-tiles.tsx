6cb68d5f66b5baf0c8aad5b2c6263329bff3fb02
Paige Ashlynn <mx.ashlynn@gmail.com>
2022-06-18 15:05:39 -0700

    Improves meteor picture in Inner Circle.

---
+++ design/tiles/meteor-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Meteor Tiles" tilewidth="8" tileheight="8" tilecount="2" columns="1">
+ <image source="meteor-tiles.png" trans="65ff00" width="8" height="16"/>
+</tileset>
